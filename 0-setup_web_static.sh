f379da17a038cf639639c08f8ef07d5782d8e3b1
Magombedze <mhizhawetswanda@gmail.com>
2023-05-07 16:10:02 -0700

    third commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,21 @@
 #!/usr/bin/env bash
-# Bash script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# setup web static server
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
+apt-get update
+apt-get install -y nginx
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared
+echo "<!DOCTYPE html>
+<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
-
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+config='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
+sed -i "38i $config" /etc/nginx/sites-available/default
+service nginx restart
+exit 0
